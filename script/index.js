3fd34cce83084ec4cdee59faf49aed60ce9680af
mizanurcodes-ux <mizanur.codes@gmail.com>
2026-03-06 20:41:41 +0600

    complate all functionlity on for LogIn page

---
+++ script/index.js
@@ -0,0 +1,17 @@
+const loginBtn = document.getElementById("loginBtn");
+const userName = document.getElementById("userName");
+const userPassword = document.getElementById("userPassword");
+
+loginBtn.addEventListener("click", function(e){
+    e.preventDefault()
+  const name = userName.value;
+  const password = userPassword.value;
+ 
+  if(name === "admin" && password === "admin123"){
+    window.location.href = "issues.html"
+  }
+  else{
+    alert( "Wrong username or password ")
+  }
+
+})
